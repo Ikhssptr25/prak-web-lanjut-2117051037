e6dbe71849ae90cd8378cacec759396bdbccd322
Ikhssptr25 <ikhsansaputra2508@gmail.com>
2023-09-25 21:31:01 +0700

    database-model

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\UserModel;
 use App\Controllers\BaseController;
 
 class UserController extends BaseController
@@ -23,17 +23,75 @@ class UserController extends BaseController
     }
 
     public function create(){
-        return view('create_user');
-    }
-
-    public function store(){
-        $data=[
-            'nama' => $this->request->getVar('nama'),
-            'kelas'  => $this->request->getVar('kelas'),
-            'npm' => $this->request->getVar('npm'),
-
+        $kelas=[
+            [
+                'id'=>1,
+                'nama_kelas'=>'A'
+            ],
+            [
+                'id'=>2,
+                'nama_kelas'=>'B'
+            ],
+            [
+                'id'=>3,
+                'nama_kelas'=>'C'
+            ],
+            [
+                'id'=>4,
+                'nama_kelas'=>'D'
+            ],
         ];
-        return view('profile',$data);
+
+        $data=[
+            'kelas' => $kelas,
+            'validation' => \Config\Services::validation()
+        ];
+        return view('create_user',$data);
     }
+
+   
+    public function store()
+{
+    $validationRules = [
+        'nama' => [
+            'rules' => 'required',
+            'errors' => [
+                'required' => '{field} mahasiswa harus di isi.'
+            ]
+        ],
+        'npm' => [
+            'rules' => 'required|is_unique[user.npm]',
+            'errors' => [
+                'required' => '{field} mahasiswa harus di isi.',
+                'is_unique' => '{field} mahasiswa sudah terdaftar.'
+            ]
+        ]
+    ];
+
+    if (!$this->validate($validationRules)) {
+        // Store validation errors and old input in flash data
+        $session = session();
+        $session->setFlashdata('validationErrors', $this->validator->getErrors());
+        $session->setFlashdata('oldInput', $this->request->getPost());
+        return redirect()->to(base_url('/user/create'));
+    }
+
+    // Validation passed, save the user
+    $userModel = new UserModel();
+    $userModel->saveUser([
+        'nama' => $this->request->getVar('nama'),
+        'id_kelas' => $this->request->getVar('kelas'),
+        'npm' => $this->request->getVar('npm'),
+    ]);
+
+    $data = [
+        'nama' => $this->request->getVar('nama'),
+        'kelas' => $this->request->getVar('kelas'),
+        'npm' => $this->request->getVar('npm'),
+    ];
+
+    return view('profile', $data);
+}
+
 }
 

3ef4edfe9c1125ed7cbb288476bec40b324b1017
Ikhssptr25 <ikhsansaputra2508@gmail.com>
2023-10-03 08:14:56 +0700

    read data

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create User</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
+<?= $this->extend('layouts/app')?>
+<?=$this->section('content')?>
+
     <div class="d-flex justify-content-center align-items-center flex-column" style="width: 100vw; height: 100vh; background: none;">
         <h2>Biodata</h2>
         <form class="d-flex flex-column mt-3" action="<?= base_url('/user/store') ?>" method="POST" style="border: 5px solid yellow; padding: 30px;">
@@ -43,5 +37,4 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+<?= $this->endSection()?>
